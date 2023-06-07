879bc70c17fdcbdead8e313b3e09827981eafc43
phillipchibaya <chibayaphillip@gmail.com>
2023-06-07 14:27:17 -0700

    Task 4

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,6 @@
+// toggles the class of the HTML tag HEADER
+// when the user clicks on the tag DIV#toggle_header
+$('DIV#toggle_header').click(() => {
+  $('HEADER').toggleClass('green');
+  $('HEADER').toggleClass('red');
+});
